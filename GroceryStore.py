4f843240cd7f4245532dea7e2fd0df616553d96f
Shandra Morton <ShanMorton@gmail.com>
2019-12-18 14:17:46 -0600

    Cleaning up MasterBranch by removing files

--- GroceryStore.py
+++
@@ -1,9 +0,0 @@
-# This is a new line to test
-penne = 16.68
-sauce = 6.98
-garlic = 16.78
-seasoning = 15.26
-baguettes = 3.00
-meatballs = 4.39
-totalsum = (penne + sauce + garlic + seasoning + baguettes + meatballs)
-print(round(totalsum, 2))
